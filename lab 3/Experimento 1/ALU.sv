186bef809ecc2b9cc983081116b04b1b799d7a74
Calquito <39011234+Calquito@users.noreply.github.com>
2021-09-06 21:26:57 -0600

    fix

--- lab 3/Experimento 1/ALU.sv
+++ lab 3/Experimento 1/ALU.sv
@@ -2,61 +2,112 @@
 ----------------------------------------------------------------------
 |ALU_Sel|   ALU Operation
 ----------------------------------------------------------------------
-| 0000  |   ALU_Out = A + B;
+| 000  |   ALU_Out = A + B;
 ----------------------------------------------------------------------
-| 0001  |   ALU_Out = A - B;
+| 001  |   ALU_Out = A - B;
 ----------------------------------------------------------------------
-| 0100  |   ALU_Out = A << 1;
+| 010  |   ALU_Out = A << 1;
 ----------------------------------------------------------------------
-| 0101  |   ALU_Out = A >> 1;
+| 011  |   ALU_Out = A >> 1;
 ----------------------------------------------------------------------
-| 1000  |   ALU_Out = A and B;
+| 100  |   ALU_Out = A and B;
 ----------------------------------------------------------------------
-| 1001  |   ALU_Out = A or B;
+| 101  |   ALU_Out = A or B;
 ----------------------------------------------------------------------
-| 1010  |   ALU_Out = A xor B;
+| 110  |   ALU_Out = A xor B;
 ----------------------------------------------------------------------*/
 
 
 
-module ALU #( parameter BITS=4)( input logic [3:0]ALU_Code,
+module ALU #( parameter BITS=4)( input logic [2:0]ALU_Code,
 
 	input logic [BITS-1:0]A,
 	input logic [BITS-1:0]B,
 	
 	output logic [BITS-1:0]ALU_Result,
-	//output logic [BITS-1:0]tmp_adder,
-	//output logic [BITS-1:0]tmp_substractor,
-	output logic [3:0]flags
+
+	//
+	output logic [3:0]flags,
+	output logic [6:0]display
+	
 	);
 	
-	
+	//addition and substraction
+	reg[3:0] tmp_adder;
+	reg[3:0] tmp_substractor;
 	N_bit_adder sumador(A,B,tmp_adder);
 	N_bit_substractor restador(A,B,tmp_substractor);
 	
+	
+	
+
+	
+	
 	always @(*)
     begin
+	 	  flags = 4'b0000;
         case(ALU_Code)
-        4'b0000: // Addition
+        3'b000: // Addition
 			  //N_bit_adder sumador(A,B,ALU_Result);
-			  ALU_Result = A + B ;
-           //ALU_Result = tmp_adder ; 
-        4'b0001: // Subtraction
+			  //ALU_Result = A + B ;
+           ALU_Result = tmp_adder ; 
+        3'b001: // Subtraction
 			  //N_bit_substractor restador(A,B,ALU_Result);
-			  ALU_Result = A - B ;
-           //ALU_Result = tmp_substractor ;
-        4'b0100: // Logical shift left
+			  //ALU_Result = A - B ;
+           ALU_Result = tmp_substractor ;
+        3'b010: // Logical shift left
            ALU_Result = A<<1;
-        4'b0101: // Logical shift right
+        3'b011: // Logical shift right
            ALU_Result = A>>1;
-        4'b1000: //  Logical and 
+        3'b100: //  Logical and 
            ALU_Result = A & B;
-        4'b1001: //  Logical or
+        3'b101: //  Logical or
            ALU_Result = A | B;
-        4'b1010: //  Logical xor 
+        3'b110: //  Logical xor 
            ALU_Result = A ^ B;
 		  default: ALU_Result = A | B;
         endcase
+		  
+		  //translate 4 bits to 7 segments display
+		  
+		  case (ALU_Result)
+			4'b0000 :      	//Hexadecimal 0
+			display = 7'b1111110;
+			4'b0001 :    		//Hexadecimal 1
+			display = 7'b0110000  ;
+			4'b0010 :  		// Hexadecimal 2
+			display = 7'b1101101 ; 
+			4'b0011 : 		// Hexadecimal 3
+			display = 7'b1111001 ;
+			4'b0100 :		// Hexadecimal 4
+			display = 7'b0110011 ;
+			4'b0101 :		// Hexadecimal 5
+			display = 7'b1011011 ;  
+			4'b0110 :		// Hexadecimal 6
+			display = 7'b1011111 ;
+			4'b0111 :		// Hexadecimal 7
+			display = 7'b1110000;
+			4'b1000 :     		 //Hexadecimal 8
+			display = 7'b1111111;
+			4'b1001 :    		//Hexadecimal 9
+			display = 7'b1111011 ;
+			4'b1010 :  		// Hexadecimal A
+			display = 7'b1110111 ; 
+			4'b1011 : 		// Hexadecimal B
+			display = 7'b0011111;
+			4'b1100 :		// Hexadecimal C
+			display = 7'b1001110 ;
+			4'b1101 :		// Hexadecimal D
+			display = 7'b0111101 ;
+			4'b1110 :		// Hexadecimal E
+			display = 7'b1001111 ;
+			4'b1111 :		// Hexadecimal F
+			display = 7'b1000111 ;
+			endcase
+			
+			flags[0]=1;
+			flags[2]=1;
+		  
 	 end
 	
 
--- lab 3/Experimento 1/simulation/modelsim/Informe_3_run_msim_rtl_verilog.do.bak11
+++ lab 3/Experimento 1/simulation/modelsim/Informe_3_run_msim_rtl_verilog.do.bak11
@@ -5,12 +5,12 @@ if {[file exists rtl_work]} {
 vlib rtl_work
 vmap work rtl_work
 
-vlog -sv -work work +incdir+C:/Users/Pc/Desktop/lab\ 3 {C:/Users/Pc/Desktop/lab 3/ALU.sv}
-vlog -sv -work work +incdir+C:/Users/Pc/Desktop/lab\ 3 {C:/Users/Pc/Desktop/lab 3/N_bit_adder.sv}
-vlog -sv -work work +incdir+C:/Users/Pc/Desktop/lab\ 3 {C:/Users/Pc/Desktop/lab 3/ALU_tb.sv}
-vlog -sv -work work +incdir+C:/Users/Pc/Desktop/lab\ 3 {C:/Users/Pc/Desktop/lab 3/N_bit_substractor.sv}
+vlog -sv -work work +incdir+C:/Users/Pc/Desktop/TallerDD/lab\ 3/Experimento\ 1 {C:/Users/Pc/Desktop/TallerDD/lab 3/Experimento 1/ALU.sv}
+vlog -sv -work work +incdir+C:/Users/Pc/Desktop/TallerDD/lab\ 3/Experimento\ 1 {C:/Users/Pc/Desktop/TallerDD/lab 3/Experimento 1/N_bit_adder.sv}
+vlog -sv -work work +incdir+C:/Users/Pc/Desktop/TallerDD/lab\ 3/Experimento\ 1 {C:/Users/Pc/Desktop/TallerDD/lab 3/Experimento 1/ALU_tb.sv}
+vlog -sv -work work +incdir+C:/Users/Pc/Desktop/TallerDD/lab\ 3/Experimento\ 1 {C:/Users/Pc/Desktop/TallerDD/lab 3/Experimento 1/N_bit_substractor.sv}
 
-vlog -sv -work work +incdir+C:/Users/Pc/Desktop/lab\ 3 {C:/Users/Pc/Desktop/lab 3/tb_N_bit_adder.sv}
+vlog -sv -work work +incdir+C:/Users/Pc/Desktop/TallerDD/lab\ 3/Experimento\ 1 {C:/Users/Pc/Desktop/TallerDD/lab 3/Experimento 1/tb_N_bit_adder.sv}
 
 vsim -t 1ps -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L cyclonev_ver -L cyclonev_hssi_ver -L cyclonev_pcie_hip_ver -L rtl_work -L work -voptargs="+acc"  tb_N_bit_adder.sv
 
